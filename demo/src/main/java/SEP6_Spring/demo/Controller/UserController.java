5303697fc0aaa34e8d491e46609e9da4fecc070d
Tamas12345678 <43003631+Tamas12345678@users.noreply.github.com>
2021-05-25 18:58:04 +0200

    CreateProject
    Added Dependencies and basic structure

---
+++ demo/src/main/java/SEP6_Spring/demo/Controller/UserController.java
@@ -0,0 +1,16 @@
+package SEP6_Spring.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/Test")
+    public String Test()
+    {
+        return "Hello World";
+    }
+
+
+}
