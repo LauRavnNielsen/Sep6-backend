ac992cf5803192900685af887dedc646a6d84229
Tamas12345678 <43003631+Tamas12345678@users.noreply.github.com>
2021-06-01 10:14:22 +0200

    Google SQL connection

--- rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -16,68 +16,6 @@ public class RestServiceApplication {
 	public static void main(String[] args) throws SQLException, IOException {
 		SpringApplication.run(RestServiceApplication.class, args);
 
-	/*	MovieAPI movieAPI = new MovieAPI();
-		movieAPI.getMovieInfoFromAPI("Inception");
-
-	 */
-
-
-
-
-
-
-
-
-
-
-
-
-		ControllerDAOImplementation controllerDAOImplementation = new ControllerDAOImplementation();
-		//controllerDAOImplementation.AddMovieToMovieList(controller,"It","admin");
-		List<Movie> movieList = controllerDAOImplementation.GetAllMovieForMovieList("admin","hate");
-
-		//System.out.println("Debug movieList: " + movieList);
-
-		CommentDAOImplementation commentDAOImplementation = new CommentDAOImplementation();
-
-		List<Comment> commentList = commentDAOImplementation.GetAllCommentsForMovie("lol");
-
-		System.out.println("Debug commentList: " + commentList);
-
-
-	/*	Rate rate = new Rate();
-		rate.setRating(9.0f);
-
-		RateDAOImplementation rateDAOImplementation = new RateDAOImplementation();
-	//	Float number = rateDAOImplementation.getRatingOnMovie("Lau","Aladin");
-
-		//System.out.println("Debug getting rate: " + number);
-
-		rateDAOImplementation.addRatingToMovie(rate, "KingKong","tomi");
-
-	 */
-
-
-
-
-
-		Comment comment = new Comment();
-		comment.setComment("Thresh.");
-
-		CommentDAOImplementation commentDAo = new CommentDAOImplementation();
-
-
-		//Add Comment to movie
-
-		commentDAo.AddCommentToMovie(comment,"Nemo","admin");
-
-		//Register user to the database
-		//	userDao.Registry(user);
-
-		//Login Test with correct given info and without
-
-
-
 
 	}
 
